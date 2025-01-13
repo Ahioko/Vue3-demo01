df0c08be4bd40866275e86985882e63524c344e7
Ahioko <chuanyun34@gmail.com>
2025-01-13 17:57:52 +0800

    scss文件的自动导入

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import '@/styles/commons.scss'
+import '@/styles/common.scss'
 
 //测试接口函数
 import { getCategory } from './apis/testApi'
